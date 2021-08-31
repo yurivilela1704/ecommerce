7eaff03a2f15ea48d9307c5cdb040369599ef295
yurivilela <yurimarquez1704@gmail.com>
2021-08-31 16:35:41 -0300

    tela de login, validacoes de sessao e rotas

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php 
-
+<?php
+session_start();
 //chama as dependencias
 require_once("vendor/autoload.php");
 
@@ -7,6 +7,9 @@ use \Slim\Slim;
 use \Hcode\DB;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model;
+
 
 $app = new Slim();
 
@@ -22,13 +25,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+    User::verifyLogin();
+
     $page = new PageAdmin();
 
     $page->setTpl("index");
 
 });
 
+$app->get('/admin/login', function () {
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("login");
+});
+
+$app->post('/admin/login', function () {
+
+
+    User::login($_POST["login"], $_POST["password"]);
+
+    header("Location: /admin");
+    exit;
+});
+
+$app->get('/admin/logout', function () {
+
+    User::logout();
+
+    header("Location: /admin/login");
+    exit;
+
+});
 
 $app->run();
-
-?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page
     private  $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data" => []
     ];
 
@@ -31,7 +33,7 @@ class Page
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
     }
 
     private function setData($data = [])
@@ -52,6 +54,6 @@ class Page
     //ultimo metodo a ser executado ao instancionar o objeto dessa classe
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 }
