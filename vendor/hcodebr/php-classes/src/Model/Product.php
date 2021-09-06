87051f6ab3458ec0241e269d8c7706229b850d1b
yurivilela <yurimarquez1704@gmail.com>
2021-09-06 13:32:04 -0300

    parte de produtos na home formatada para receber dados direto do banco

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -15,6 +15,18 @@ class Product extends Model
 
     }
 
+    public static function checkList($list)
+    {
+
+        foreach ($list as &$row) {
+
+            $product = new Product();
+            $product->setData($row);
+            $row = $product->getValues();
+        }
+        return $list;
+    }
+
     public function save()
     {
         $sql = new Sql();
